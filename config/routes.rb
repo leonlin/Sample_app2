793dcce8c07635f1d1968298c2a3ca90a909b9b2
kyk <linjiansuper@gmail.com>
2012-03-24 22:47:52 +0800

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 SampleApp::Application.routes.draw do
-
   root to: 'static_pages#home'
+  resources :sessions, only: [:new, :create, :destroy]
+
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   match '/signup',  to: 'users#new'
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -34,6 +34,14 @@ describe "User pages" do
         end.to change(User, :count).by(1)
 
       end
+
+      describe "after saving the user" do
+        it { should have_link('Sign out') }
+      end
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
     end
   end
   describe "profile page" do
